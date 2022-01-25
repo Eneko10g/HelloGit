2fa9c2e532a45e65dfea9bf4fdf942b4480ea1c4
docencia <docencia@U033328.vp.ehu.es>
2022-01-25 16:40:30 +0100

    Cambiado el mensaje en la rama beta

--- src/MiPrimeraClase.java
+++ src/MiPrimeraClase.java
@@ -2,8 +2,7 @@
 public class MiPrimeraClase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Kaixo");
+		System.out.println("Hello!!!");
 	}
 
 }
