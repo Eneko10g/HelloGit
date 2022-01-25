a6a006d5504e0e566dc13da6507e5f3419f90ae6
docencia <docencia@U033328.vp.ehu.es>
2022-01-25 16:43:25 +0100

    Otro cambio en la rama principal

--- src/MiPrimeraClase.java
+++ src/MiPrimeraClase.java
@@ -3,7 +3,7 @@ public class MiPrimeraClase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Kaixo");
+		System.out.println("Beste mezu bat");
 	}
 
 }
