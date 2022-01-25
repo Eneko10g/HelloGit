dd18ada3155f1d346d0c156f97008f5aec755ad2
docencia <docencia@U033328.vp.ehu.es>
2022-01-25 16:41:42 +0100

    un nuevo método

--- src/MiPrimeraClase.java
+++ src/MiPrimeraClase.java
@@ -4,5 +4,7 @@ public class MiPrimeraClase {
 	public static void main(String[] args) {
 		System.out.println("Hello!!!");
 	}
-
+	public static void otroMetodo() {
+		System.out.println("Otro mensaje");
+	}
 }
